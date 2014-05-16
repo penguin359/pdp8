007b7b195f3a0236c33cacddcaa2d1dd8ccf625e
Loren M. Lang <lorenl@north-winds.org>
2014-05-15 19:33:23 -0700

    More instructions in test suite

--- test_cpu.vhd
+++ test_cpu.vhd
@@ -134,6 +134,109 @@ begin
 		wait for clk_period;
 		mem_valid <= '0';
 
+		-- Read instruction
+		wait for clk_period*4;
+		assert mem_read = '1'
+		report "(3)CPU Not reading instruction"
+		severity failure;
+		assert addr = "111000000000"
+		report "(3a)Incorrect address"
+		severity failure;
+
+		-- TAD 000 0011
+		din <= "001010000011";
+		mem_valid <= '1';
+		wait for clk_period;
+		mem_valid <= '0';
+		assert mem_read = '0'
+		report "(3)Read not finished"
+		severity failure;
+
+		-- Operand read 14
+		wait for clk_period*4;
+		assert mem_read = '1'
+		report "(3)Not reading operand"
+		severity failure;
+		assert addr = "111000000011"
+		report "(3b)Incorrect address"
+		severity failure;
+		din <= "000000001110";
+		mem_valid <= '1';
+		wait for clk_period;
+		mem_valid <= '0';
+
+		-- Read instruction
+		wait for clk_period*4;
+		assert mem_read = '1'
+		report "(4)CPU Not reading instruction"
+		severity failure;
+		assert addr = "111000000001"
+		report "(4a)Incorrect address"
+		severity failure;
+
+		-- DCA Z 000 1000
+		din <= "011000001000";
+		mem_valid <= '1';
+		wait for clk_period;
+		mem_valid <= '0';
+		assert mem_read = '0'
+		report "(4)Read not finished"
+		severity failure;
+
+		-- Operand read 14
+		wait for clk_period*4;
+		assert mem_read = '0'
+		report "(4)Incorrectly reading operand"
+		severity failure;
+		assert mem_write = '1'
+		report "(4)Not writing operand"
+		severity failure;
+		assert addr = "000000001000"
+		report "(4b)Incorrect address"
+		severity failure;
+		assert dout = "000000101000"
+		report "(4)Storing wrong value"
+		severity failure;
+		mem_valid <= '1';
+		wait for clk_period;
+		mem_valid <= '0';
+
+		-- Read instruction
+		wait for clk_period*4;
+		assert mem_read = '1'
+		report "(5)CPU Not reading instruction"
+		severity failure;
+		assert addr = "111000000010"
+		report "(5a)Incorrect address"
+		severity failure;
+
+		-- DCA 000 1011
+		din <= "011010001011";
+		mem_valid <= '1';
+		wait for clk_period;
+		mem_valid <= '0';
+		assert mem_read = '0'
+		report "(5)Read not finished"
+		severity failure;
+
+		-- Operand read 14
+		wait for clk_period*4;
+		assert mem_read = '0'
+		report "(5)Incorrectly reading operand"
+		severity failure;
+		assert mem_write = '1'
+		report "(5)Not writing operand"
+		severity failure;
+		assert addr = "111000001011"
+		report "(5b)Incorrect address"
+		severity failure;
+		assert dout = "000000000000"
+		report "(5)Storing wrong value"
+		severity failure;
+		mem_valid <= '1';
+		wait for clk_period;
+		mem_valid <= '0';
+
 		wait;
 	end process;
 
