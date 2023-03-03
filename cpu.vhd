781c5577b8fa5d30ae2640342e2d06cae4d19be5
Loren M. Lang <lorenl@north-winds.org>
2023-03-02 23:30:33 -0800

    Updated CPU interface to match the EE432 class specification exactly

--- cpu.vhd
+++ cpu.vhd
@@ -2,33 +2,68 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+--use IEEE.NUMERIC_STD.ALL;
 use work.control.ALL;
 ---------------------------------------------------------------------
 entity cpu is
-    Port( clk : in  STD_LOGIC;
-	  din : in STD_LOGIC_VECTOR(11 downto 0);
-	  dout : out STD_LOGIC_VECTOR(11 downto 0);
-	  addr : out STD_LOGIC_VECTOR(11 downto 0);
-	  mem_read : out STD_LOGIC;
-	  mem_write : out STD_LOGIC;
-	  mem_valid : in STD_LOGIC;
-	  iot_skip : in STD_LOGIC;
-	  iot_ac_zero : in STD_LOGIC;
-	  iot_din : in STD_LOGIC_VECTOR(11 downto 0);
-	  iot_dout : out STD_LOGIC_VECTOR(11 downto 0);
-	  iot_addr : out STD_LOGIC_VECTOR(5 downto 0);
-	  iot_bits : out STD_LOGIC_VECTOR(2 downto 0);
-	  -- Panel to CPU
-	  swreg : in STD_LOGIC_VECTOR(11 downto 0);
-	  dispsel : in STD_LOGIC_VECTOR(1 downto 0);
-	  run : in STD_LOGIC;
-	  loadpc : in STD_LOGIC;
-	  step : in STD_LOGIC;
-	  deposit : in STD_LOGIC;
-	  -- CPU to Panel
-	  dispout : out STD_LOGIC_VECTOR(11 downto 0);
-	  linkout : out STD_LOGIC;
-	  halt : out STD_LOGIC
+    --      mem_valid : in STD_LOGIC;
+    Port(
+        clk : in  STD_LOGIC;
+        nrst : in  STD_LOGIC;
+
+        -- UI Bus
+        --   Panel to external interfaces
+        --ld : out STD_LOGIC_VECTOR(7 downto 0);
+        --anodes : out STD_LOGIC_VECTOR(3 downto 0);
+        --cathodes : out STD_LOGIC_VECTOR(7 downto 0);
+
+        -- Panel Bus
+        --   Panel to CPU
+        swreg : in STD_LOGIC_VECTOR(11 downto 0);
+        dispsel : in STD_LOGIC_VECTOR(1 downto 0);
+        run : in STD_LOGIC;
+        loadpc : in STD_LOGIC;
+        step : in STD_LOGIC;
+        deposit : in STD_LOGIC;
+
+        --   CPU to Panel
+        dispout : out STD_LOGIC_VECTOR(11 downto 0);
+        linkout : out STD_LOGIC;
+        halt : out STD_LOGIC;
+
+        -- IO Bus
+        --   CPU to IOT Distributor
+        bit1_cp2 : out STD_LOGIC;
+        bit2_cp3 : out STD_LOGIC;
+        io_address : out STD_LOGIC_VECTOR(2 downto 0);
+        dataout : out STD_LOGIC_VECTOR(7 downto 0);
+
+        --   IOT Distributor to CPU
+        skip_flag : in STD_LOGIC;
+        clearacc : in STD_LOGIC;
+        datain : in STD_LOGIC_VECTOR(7 downto 0);
+
+        -- UART Bus
+        --   IOT Distributor to UART
+        --clear_3 : STD_LOGIC;
+        --load_3 : STD_LOGIC;
+        --dataout_3 : STD_LOGIC_VECTOR(7 downto 0);
+
+        --    UART to IOT Distributor
+        --ready_3 : STD_LOGIC;
+        --clearacc_3 : STD_LOGIC;
+        --datain_3 : STD_LOGIC_VECTOR(7 downto 0);
+
+        -- Memory Bus
+        --   CPU to RAM
+        address : out STD_LOGIC_VECTOR(11 downto 0);
+        write_data : out STD_LOGIC_VECTOR(11 downto 0);
+        write_enable : out STD_LOGIC;
+        mem_load : out STD_LOGIC;
+
+        --   RAM to CPU
+        read_data : in STD_LOGIC_VECTOR(11 downto 0);
+        mem_ready : in STD_LOGIC
     );
 end cpu;
 ---------------------------------------------------------------------
@@ -146,6 +181,11 @@ signal sel_md : sel_md;
 
 signal md_clear : STD_LOGIC;
 signal iot_skip2 : STD_LOGIC;
+
+signal iot_bits : STD_LOGIC_VECTOR(2 downto 0);
+
+signal mem_read, mem_write : STD_LOGIC;
+
 begin
 	inst_state: state Port Map(
 		clk => clk,
@@ -161,19 +201,20 @@ begin
 		sel_ir => sel_ir,
 		sel_ma => sel_ma,
 		sel_md => sel_md,
+		--mem_read => open,  -- TODO Should this be implemented?
 		mem_read => mem_read,
 		mem_write => mem_write,
-		mem_valid => mem_valid,
+		mem_valid => mem_ready,
 		halted => open
 	);
 
-	with sel_addr select addr <=
+	with sel_addr select address <=
 		ea when addr_ea,
 		ma when addr_ma,
 		pc when addr_pc,
 		ea when others;
 
-	with sel_data select dout <=
+	with sel_data select write_data <=
 		ac when data_ac,
 		md when data_md,
 		--pc when data_pc,
@@ -209,7 +250,7 @@ begin
 	begin
 		if rising_edge(clk) then
 			if sel_pc = pc_data then
-				pc <= din;
+				pc <= read_data;
 			elsif sel_pc = pc_ma then
 				pc <= ma;
 			elsif sel_pc = pc_ma1 then
@@ -237,10 +278,10 @@ begin
 			elsif sel_ac = ac_uc then
 				link_ac <= uc_link_ac;
 			elsif sel_ac = ac_iot then
-				if iot_ac_zero = '1' then
-					link_ac <= link & iot_din;
+				if clearacc = '1' then
+					link_ac <= link & "0000" & datain;
 				else
-					link_ac <= link & (ac OR iot_din);
+					link_ac <= link & (ac OR "0000" & datain);
 				end if;
 			end if;
 		end if;
@@ -251,7 +292,7 @@ begin
 	begin
 		if rising_edge(clk) then
 			if sel_ir = ir_data then
-				ir <= din;
+				ir <= read_data;
 			end if;
 		end if;
 	end process;
@@ -261,7 +302,7 @@ begin
 	begin
 		if rising_edge(clk) then
 			if sel_ma = ma_data then
-				ma <= din;
+				ma <= read_data;
 			elsif sel_ma = ma_ea then
 				ma <= ea;
 			end if;
@@ -273,15 +314,15 @@ begin
 	begin
 		if rising_edge(clk) then
 			if sel_md = md_data then
-				md <= din;
+				md <= read_data;
 			elsif sel_md = md_data1 then
-				md <= din + 1;
+				md <= read_data + 1;
 			end if;
 		end if;
 	end process;
 	md_clear <= '1' when md = "000000000000" else '0';
 
-	iot_skip2 <= '1' when iot_skip = '1' and ir(0) = '1' else '0';
+	iot_skip2 <= '1' when skip_flag = '1' and ir(0) = '1' else '0';
 	with sel_skip select
 	skip <= md_clear when skip_md_clear,
 		uc_skip  when skip_uc,
@@ -360,9 +401,19 @@ begin
 	ac <= link_ac(11 downto 0);
 	link <= link_ac(12);
 
-	iot_dout <= ac;
-	iot_addr <= ir(8 downto 3) when sel_iot = iot_en else "000000";
+        -- TODO Should all ac bits be supported?
+	--dataout <= ac;
+	dataout <= ac(7 downto 0);
+        -- TODO Support all address bits
+	--io_address <= ir(8 downto 3) when sel_iot = iot_en else "000000";
+	io_address <= ir(8 downto 6) when sel_iot = iot_en else "000000";
 	iot_bits <= ir(2 downto 0);
+
+        bit1_cp2 <= iot_bits(1);
+        bit2_cp3 <= iot_bits(2);
+
+        mem_load <= mem_write OR mem_read;
+        write_enable <= mem_write;
 end behavioral;
 
 --10 ... 02 01 00 12 11 RTL
