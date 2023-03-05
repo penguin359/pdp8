692099dd936ea00552b0540175154acec1a0b4a4
Loren M. Lang <lorenl@north-winds.org>
2023-03-05 03:54:10 -0800

    All non-privileged microcode instructions implemented in reference model
    No testing has been done on them beyond the no-op OPR instruction.

--- pdp8.vh
+++ pdp8.vh
@@ -1,3 +1,6 @@
+localparam A_WIDTH = 12;
+localparam D_WIDTH = 12;
+
 localparam [2:0] OPCODE_AND = 3'b000;
 localparam [2:0] OPCODE_TAD = 3'b001;
 localparam [2:0] OPCODE_ISZ = 3'b010;
@@ -9,3 +12,32 @@ localparam [2:0] OPCODE_OPR = 3'b111;
 
 localparam Z_BIT = 7;
 localparam I_BIT = 8;
+
+localparam UC_GROUP1_BIT = 8;
+localparam UC_GROUP2_BIT = 0;
+
+// uC Group 1 bits
+localparam CLA_BIT = 7;
+localparam CLL_BIT = 6;
+localparam CMA_BIT = 5;
+localparam CML_BIT = 4;
+localparam RAR_BIT = 3;
+localparam RAL_BIT = 2;
+localparam BSW_BIT = 1;
+localparam IAC_BIT = 0;
+
+// uC Group 2 bits
+//localparam CLA_BIT = 7;
+localparam SMA_BIT = 6;
+localparam SZA_BIT = 5;
+localparam SNL_BIT = 4;
+localparam AND_BIT = 3;
+localparam OSR_BIT = 2;
+localparam HLT_BIT = 1;
+
+// uC Group 3 bits
+//localparam CLA_BIT = 7;
+localparam MQA_BIT = 6;
+localparam SCA_BIT = 5;
+localparam MQL_BIT = 4;
+// 3:1 Code bits
--- tests/cpu_reference.svh
+++ tests/cpu_reference.svh
@@ -1,13 +1,11 @@
 class cpu_reference;
     `include "pdp8.vh"
 
-    localparam A_WIDTH = 12;
-    localparam D_WIDTH = 12;
-
     bit link = 1'b0;
     //bit [D_WIDTH-1:0] acc = D_WIDTH'b0;
     //bit [D_WIDTH-1:0] pc  = A_WIDTH'b0;
     bit [D_WIDTH-1:0] acc       = 12'b0;
+    bit [D_WIDTH-1:0] mq        = 12'b0;
     bit [D_WIDTH-1:0] pc        = 12'b0;
 
     bit [D_WIDTH-1:0] mem [0:2**A_WIDTH-1];
@@ -134,11 +132,107 @@ class cpu_reference;
                 state = READ_INSTR;
                 return;
             end
-            default: begin
-                if(trans.read_data != 12'b111_000_000_000)
-                    cpu_error("Unimplemented OPR or IOT!");
+            OPCODE_OPR: begin
+                if(trans.read_data[UC_GROUP1_BIT] == 1'b0) begin
+                    logic [2:0] rotate_bits;
+                    // Group 1 microcoded instruction
+                    if(trans.read_data[CLA_BIT] == 1'b1)
+                        acc = 0;
+                    if(trans.read_data[CLL_BIT] == 1'b1)
+                        link = 0;
+                    if(trans.read_data[CMA_BIT] == 1'b1)
+                        acc = ~acc;
+                    if(trans.read_data[CML_BIT] == 1'b1)
+                        link = ~link;
+                    if(trans.read_data[IAC_BIT] == 1'b1) begin
+                        logic [12:0] link_acc = {link, acc} + 13'b1;
+                        link = link_acc[12];
+                        acc = link_acc[11:0];
+                    end
+                    rotate_bits = {trans.read_data[RAL_BIT], trans.read_data[RAR_BIT], trans.read_data[BSW_BIT]};
+                    case(rotate_bits)
+                        3'b000: ;  // Nothing to do
+                        3'b100: begin  // RAL (rotate left)
+                            logic [12:0] link_acc = {acc, link};
+                            link = link_acc[12];
+                            acc = link_acc[11:0];
+                        end
+                        3'b101: begin  // RTL (rotate twice left)
+                            logic [12:0] link_acc = {acc[10:0], link, acc[11]};
+                            link = link_acc[12];
+                            acc = link_acc[11:0];
+                        end
+                        3'b010: begin  // RAR (rotate right)
+                            logic [12:0] link_acc = {acc[0], link, acc[11:1]};
+                            link = link_acc[12];
+                            acc = link_acc[11:0];
+                        end
+                        3'b011: begin  // RTR (rotate twice right)
+                            logic [12:0] link_acc = {acc[9:0], link, acc[11:10]};
+                            link = link_acc[12];
+                            acc = link_acc[11:0];
+                        end
+                        3'b001: begin  // BSW (byte swap)
+                            acc = {acc[5:0], acc[11:6]};
+                        end
+                        default:
+                            // This is a warning since these combinations
+                            // are completely unspecified in any document.
+                            `uvm_warning("CPU_REFERENCE", "Invalid microcode instruction")
+                    endcase
+                end else if(trans.read_data[UC_GROUP2_BIT] == 1'b0) begin
+                    // Group 2 microcoded instruction
+                    bit skip = 0;
+                    if(trans.read_data[AND_BIT] == 1'b0) begin
+                        // OR sub-group
+                        if(trans.read_data[SMA_BIT] == 1'b1 && acc[11] == 1'b1)
+                            skip = 1'b1;
+                        if(trans.read_data[SZA_BIT] == 1'b1 && acc == 12'b0)
+                            skip = 1'b1;
+                        if(trans.read_data[SNL_BIT] == 1'b1 && link == 1'b1)
+                            skip = 1'b1;
+                    end else begin
+                        // AND sub-group
+                        // TODO This has to have a cleaner way to write it.
+                        skip = 1'b1;
+                        if(trans.read_data[SMA_BIT] == 1'b1) begin  // SPA
+                            if(acc[11] == 1'b1)
+                                skip = 0'b1;
+                        end
+                        if(trans.read_data[SZA_BIT] == 1'b1) begin  // SNA
+                            if(acc[11] == 12'b0)
+                                skip = 0'b1;
+                        end
+                        if(trans.read_data[SNL_BIT] == 1'b1) begin  // SZL
+                            if(link == 1'b1)
+                                skip = 0'b1;
+                        end
+                    end
+                    if(skip)
+                        pc = pc + 1;
+                    if(trans.read_data[CLA_BIT] == 1'b1)
+                        acc = 0;
+                    if(trans.read_data[OSR_BIT] == 1'b1 ||
+                       trans.read_data[HLT_BIT] == 1'b1)
+                        cpu_error("Unimplemented OPR!");
+                end else begin
+                    // Group 3 microcoded instruction
+                    if(trans.read_data[CLA_BIT] == 1'b1)
+                        acc = 0;
+                    if(trans.read_data[MQA_BIT] == 1'b1)
+                        acc = acc | mq;
+                    if(trans.read_data[MQL_BIT] == 1'b1) begin
+                        mq = acc;
+                        acc = 0;
+                    end
+                    if({trans.read_data[SCA_BIT], trans.read_data[3:1]} != 4'b0)
+                        cpu_error("Unimplemented OPR!");
+                end
                 pc = pc + 1;
                 state = READ_INSTR;
+            end
+            OPCODE_IOT: begin
+                cpu_error("Unimplemented IOT!");
             end
         endcase
     endfunction
